6ef905d02db9eb4de2c7b445f02202b2e1725036
Marek Vanco <marek.vanco@laposte.net>
2011-02-09 23:48:29 +0100

    Refactoring layout, and implementation of SEARCH API

--- web_weather_graph.rb
+++ web_weather_graph.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
+require 'open-uri'
+require 'json'
 
 DATA_DIR='data/'
 URLBASE='http://www.worldweatheronline.com'
+FORMAT='json'
+KEY='31cedb71bb220950113101'
 
 
 class WebWeatherGraph < Sinatra::Base
@@ -10,8 +14,13 @@ class WebWeatherGraph < Sinatra::Base
 	set :public, File.dirname(__FILE__)+'/static'
 
 	get '/' do
-		Trencin=WeatherData.new('Trencin_Slovakia')
+		query='Trencin, Slovakia'
+		Weather=WeatherData.new
 		erb :index
+	end
+
+	post '/get_localities' do
+		localities=Weather.get_localities(params[:query])
 	end
 end
 
@@ -28,30 +37,64 @@ class WeatherData
 		end
 
 	public
-	
-	# Read all lines with last residual value
-	def initialize(locality)
-		File.open(DATA_DIR+locality,"r").each do |line|
-			data=line.split("\t")
-
-			@observation_time=data[0]
-			@cloudcover=data[7]
-			@humidity=data[4]
-			@precip_mm=data[6]
-			@presure=data[5]
-			@temp_c=data[2]
-			@temp_f=data[3]
-			@visibility=data[8]
-			@weather_code=data[1]
-			@weather_desc=data
-			@weather_icon_url=data[13][0..-2]
-			@winddir_16_point=data[9]
-			@winddir_degree=data[10]
-			@windspeed_kmph=data[11]
-			@windspeed_miles=data[12]
+		def initialize
+			@observation_time=''
+			@cloudcover=''
+			@humidity=''
+			@precip_mm=''
+			@presure=''
+			@temp_c=''
+			@temp_f=''
+			@visibility=''
+			@weather_code=''
+			@weather_desc=''
+			@weather_icon_url=''
+			@winddir_16_point=''
+			@winddir_degree=''
+			@windspeed_kmph=''
+			@windspeed_miles=''
 		end
-		@locality=normalize_locality(locality)
-	end
+
+		# Read all lines with last residual value
+		def get_data(locality)
+			File.open(DATA_DIR+locality,"r").each do |line|
+				data=line.split("\t")
+
+				@observation_time=data[0]
+				@cloudcover=data[7]
+				@humidity=data[4]
+				@precip_mm=data[6]
+				@presure=data[5]
+				@temp_c=data[2]
+				@temp_f=data[3]
+				@visibility=data[8]
+				@weather_code=data[1]
+				@weather_desc=data
+				@weather_icon_url=data[13][0..-2]
+				@winddir_16_point=data[9]
+				@winddir_degree=data[10]
+				@windspeed_kmph=data[11]
+				@windspeed_miles=data[12]
+			end
+			@locality=normalize_locality(locality)
+		end
+
+		def get_localities(query)
+			@url="#{URLBASE}/feed/search.ashx?key=#{KEY}&q=#{query}&format=#{FORMAT}&num_of_days=2"
+
+			rawdata=''
+			open(@url).each do |line|
+				rawdata << line
+			end
+			return data=JSON.parse(rawdata)
+			
+#			@city=data["search_api"]["result"][0]["areaName"][0]["value"]
+#			@country=data["search_api"]["result"][0]["country"][0]["value"]
+#			@latitude=data["search_api"]["result"][0]["latitude"]
+#			@longitude=data["search_api"]["result"][0]["longitude"]
+#			@population=data["search_api"]["result"][0]["population"]
+#			@weatherUrl=data["search_api"]["result"][0]["weatherUrl"][0]["value"]
+		end	 
 end
 
 
