8f27f348c599c866e195d62f88a45a093e3a3129
Marek Vanco <marek.vanco@laposte.net>
2011-02-13 00:43:03 +0100

    Add search city API. Need be refactored.

--- collector.rb
+++ collector.rb
@@ -4,124 +4,124 @@ require 'rubygems'
 require 'open-uri'
 require 'json'
 
-KEY='31cedb71bb220950113101'
-FORMAT='json'
-URLBASE='http://www.worldweatheronline.com'
-DATA_DIR="data/"
+KEY = '31cedb71bb220950113101'
+FORMAT = 'json'
+URLBASE = 'http://www.worldweatheronline.com'
+DATA_DIR = "data/"
 
 class Collector
 
-	attr_accessor :locality, :url, :localtime
-	attr_accessor :observation_time, :cloudcover, :humidity, :precip_mm, :presure, :temp_c, :temp_f, :visibility, :weather_code, :weather_desc, 
-								:weather_icon_url, :winddir_16_point, :winddir_degree, :windspeed_kmph, :windspeed_miles 
-	attr_accessor :date, :temp_max_c, :temp_max_f, :temp_min_c, :temp_min_f, :day_weather_code, :day_weather_desc, :day_weather_icon_url, 
-								:day_winddir_16_point, :day_winddir_degree, :day_winddirection, :day_winspeed_kmph, :day_winspeed_miles
+  attr_accessor :locality, :url, :localtime
+  attr_accessor :observation_time, :cloudcover, :humidity, :precip_mm, :presure, :temp_c, :temp_f, :visibility, :weather_code, :weather_desc, 
+                :weather_icon_url, :winddir_16_point, :winddir_degree, :windspeed_kmph, :windspeed_miles 
+  attr_accessor :date, :temp_max_c, :temp_max_f, :temp_min_c, :temp_min_f, :day_weather_code, :day_weather_desc, :day_weather_icon_url, 
+                :day_winddir_16_point, :day_winddir_degree, :day_winddirection, :day_winspeed_kmph, :day_winspeed_miles
 
-	def initialize(locality)
-		@locality=locality
-		@url="#{URLBASE}/feed/weather.ashx?key=#{KEY}&q=#{locality}&format=#{FORMAT}&num_of_days=2"
-	end
+  def initialize(locality)
+    @locality = locality
+    @url = "#{URLBASE}/feed/weather.ashx?key = #{KEY}&q = #{locality}&format = #{FORMAT}&num_of_days = 2"
+  end
 
-	private
-	  # Define a correct filename from location name
-		def normalize_filename(locality)
-		  return locality.gsub!(/[^0-9A-Za-z.\-]/, '_')
-		end
+  private
+    # Define a correct filename from location name
+    def normalize_filename(locality)
+      return locality.gsub!(/[^0-9A-Za-z.\-]/, '_')
+    end
 
-		# Change time to standard rubytime
-		def normalize_time
-			hour=observation_time[0..1]
-			minute=observation_time[3..4]
-			ampm=observation_time[6]
-			year=date[0..3]
-			mounth=date[5..6]
-			day=date[8..9]
+    # Change time to standard rubytime
+    def normalize_time
+      hour = observation_time[0..1]
+      minute = observation_time[3..4]
+      ampm = observation_time[6]
+      year = date[0..3]
+      mounth = date[5..6]
+      day = date[8..9]
       
-			if ampm=='P' 
-				hour=@observation_time[0..1].to_i
-				hour=hour+12
-			end
+      if ampm =  = 'P' 
+        hour = @observation_time[0..1].to_i
+        hour = hour+12
+      end
 
-			local_time=Time.now
-			return Time.utc(year,mounth,day,hour,minute)
-		end
+      local_time = Time.now
+      return Time.utc(year,mounth,day,hour,minute)
+    end
 
-		def normalize_weather_icon_url
-			return weather_icon_url.split("/")[5] 
-		end
+    def normalize_weather_icon_url
+      return weather_icon_url.split("/")[5] 
+    end
 
 
-	public
-	def get_values
-		rawdata=''
-		open(@url).each do |line|
-			rawdata << line
-		end
+  public
+  def get_values
+    rawdata = ''
+    open(@url).each do |line|
+      rawdata << line
+    end
 
-		data=JSON.parse(rawdata)
-		@observation_time=data["data"]["current_condition"][0]["observation_time"]
-		@cloudcover=data["data"]["current_condition"][0]["cloudcover"]
-		@humidity=data["data"]["current_condition"][0]["humidity"]
-		@precip_mm=data["data"]["current_condition"][0]["precipMM"]
-		@presure=data["data"]["current_condition"][0]["pressure"]
-		@temp_c=data["data"]["current_condition"][0]["temp_C"]
-		@temp_f=data["data"]["current_condition"][0]["temp_F"]
-		@visibility=data["data"]["current_condition"][0]["visibility"]
-		@weather_code=data["data"]["current_condition"][0]["weatherCode"]
-		@weather_desc=data["data"]["current_condition"][0]["weatehrDesc"]
-		@weather_icon_url=data["data"]["current_condition"][0]["weatherIconUrl"][0]["value"]
-		@winddir_16_point=data["data"]["current_condition"][0]["winddir16Point"]
-		@winddir_degree=data["data"]["current_condition"][0]["winddirDegree"]
-		@windspeed_kmph=data["data"]["current_condition"][0]["windspeedKmph"]
-		@windspeed_miles=data["data"]["current_condition"][0]["windspeedMiles"]
+    data = JSON.parse(rawdata)
+    @observation_time = data["data"]["current_condition"][0]["observation_time"]
+    @cloudcover = data["data"]["current_condition"][0]["cloudcover"]
+    @humidity = data["data"]["current_condition"][0]["humidity"]
+    @precip_mm = data["data"]["current_condition"][0]["precipMM"]
+    @presure = data["data"]["current_condition"][0]["pressure"]
+    @temp_c = data["data"]["current_condition"][0]["temp_C"]
+    @temp_f = data["data"]["current_condition"][0]["temp_F"]
+    @visibility = data["data"]["current_condition"][0]["visibility"]
+    @weather_code = data["data"]["current_condition"][0]["weatherCode"]
+    @weather_desc = data["data"]["current_condition"][0]["weatehrDesc"]
+    @weather_icon_url = data["data"]["current_condition"][0]["weatherIconUrl"][0]["value"]
+    @winddir_16_point = data["data"]["current_condition"][0]["winddir16Point"]
+    @winddir_degree = data["data"]["current_condition"][0]["winddirDegree"]
+    @windspeed_kmph = data["data"]["current_condition"][0]["windspeedKmph"]
+    @windspeed_miles = data["data"]["current_condition"][0]["windspeedMiles"]
 
-		@date=data["data"]["weather"][0]["date"]
-		@temp_max_c=data["data"]["weather"][0]["tempMaxC"]
-		@temp_max_f=data["data"]["weather"][0]["tempMaxF"]
-		@temp_min_c=data["data"]["weather"][0]["tempMinC"]
-		@temp_min_f=data["data"]["weather"][0]["tempMinF"]
-		@day_weather_code=data["data"]["weather"][0]["weatherCode"]
-		@day_weather_desc=data["data"]["weather"][0]["weatherDesc"][0]["value"]
-		@day_weather_icon_url=data["data"]["weather"][0]["weatherIconUrl"][0]["value"]
-		@day_winddir_16_point=data["data"]["weather"][0]["winddir16Point"]
-		@day_winddir_degree=data["data"]["weather"][0]["winddirDegree"]
-		@day_winddirection=data["data"]["weather"][0]["windDirection"]
-		@day_winspeed_kmph=data["data"]["weather"][0]["windspeedKmph"]
-		@day_winspeed_miles=data["data"]["weather"][0]["windspeedMiles"]
+    @date = data["data"]["weather"][0]["date"]
+    @temp_max_c = data["data"]["weather"][0]["tempMaxC"]
+    @temp_max_f = data["data"]["weather"][0]["tempMaxF"]
+    @temp_min_c = data["data"]["weather"][0]["tempMinC"]
+    @temp_min_f = data["data"]["weather"][0]["tempMinF"]
+    @day_weather_code = data["data"]["weather"][0]["weatherCode"]
+    @day_weather_desc = data["data"]["weather"][0]["weatherDesc"][0]["value"]
+    @day_weather_icon_url = data["data"]["weather"][0]["weatherIconUrl"][0]["value"]
+    @day_winddir_16_point = data["data"]["weather"][0]["winddir16Point"]
+    @day_winddir_degree = data["data"]["weather"][0]["winddirDegree"]
+    @day_winddirection = data["data"]["weather"][0]["windDirection"]
+    @day_winspeed_kmph = data["data"]["weather"][0]["windspeedKmph"]
+    @day_winspeed_miles = data["data"]["weather"][0]["windspeedMiles"]
     
-		@observation_time=normalize_time
-		@weather_icon_url=normalize_weather_icon_url
-	end
+    @observation_time = normalize_time
+    @weather_icon_url = normalize_weather_icon_url
+  end
 
-	def save_values
-		p dataline="#{@observation_time}\t#{weather_code}\t#{temp_c}\t#{temp_f}\t#{humidity}\t#{presure}\t#{precip_mm}\t#{cloudcover}\t#{visibility}\t#{winddir_16_point}\t#{winddir_degree}\t#{windspeed_kmph}\t#{windspeed_miles}\t#{weather_icon_url}\n"
-		f=File.new(DATA_DIR+normalize_filename(locality), "a+")
-		f.write(dataline)
-		f.close		
-	end
+  def save_values
+    p dataline = "#{@observation_time}\t#{weather_code}\t#{temp_c}\t#{temp_f}\t#{humidity}\t#{presure}\t#{precip_mm}\t#{cloudcover}\t#{visibility}\t#{winddir_16_point}\t#{winddir_degree}\t#{windspeed_kmph}\t#{windspeed_miles}\t#{weather_icon_url}\n"
+    f = File.new(DATA_DIR+normalize_filename(locality), "a+")
+    f.write(dataline)
+    f.close    
+  end
 end
 
 class Weather
-	attr_accessor :city, :country, :latitude, :longitude, :population, :weather_url
+  attr_accessor :city, :country, :latitude, :longitude, :population, :weather_url
 
-	def get_locality(locality)
-		@url="#{URLBASE}/feed/search.ashx?key=#{KEY}&q=#{locality}&format=#{FORMAT}&num_of_days=2"
+  def get_locality(locality)
+    @url = "#{URLBASE}/feed/search.ashx?key = #{KEY}&q = #{locality}&format = #{FORMAT}&num_of_days = 2"
 
-		rawdata=''
-		open(@url).each do |line|
-			rawdata << line
-		end
-		p data=JSON.parse(rawdata)
-		
-		@city=data["search_api"]["result"][0]["areaName"][0]["value"]
-		@country=data["search_api"]["result"][0]["country"][0]["value"]
-		@latitude=data["search_api"]["result"][0]["latitude"]
-		@longitude=data["search_api"]["result"][0]["longitude"]
-		@population=data["search_api"]["result"][0]["population"]
-		@weatherUrl=data["search_api"]["result"][0]["weatherUrl"][0]["value"]
-	end	
+    rawdata = ''
+    open(@url).each do |line|
+      rawdata << line
+    end
+    p data = JSON.parse(rawdata)
+    
+    @city = data["search_api"]["result"][0]["areaName"][0]["value"]
+    @country = data["search_api"]["result"][0]["country"][0]["value"]
+    @latitude = data["search_api"]["result"][0]["latitude"]
+    @longitude = data["search_api"]["result"][0]["longitude"]
+    @population = data["search_api"]["result"][0]["population"]
+    @weatherUrl = data["search_api"]["result"][0]["weatherUrl"][0]["value"]
+  end  
 end
 
-Trencin=Collector.new("Trencin,Slovakia")
+Trencin = Collector.new("Trencin,Slovakia")
 Trencin.get_values
 Trencin.save_values
--- web_weather_graph.rb
+++ web_weather_graph.rb
@@ -1,137 +1,103 @@
 require 'sinatra'
 require 'open-uri'
 require 'json'
+require 'erb'
 
-DATA_DIR='data/'
-URLBASE='http://www.worldweatheronline.com'
-FORMAT='json'
-KEY='31cedb71bb220950113101'
-
-
-class WebWeatherGraph < Sinatra::Base
-	set :static, true
-	set :logging, true
-	set :public, File.dirname(__FILE__)+'/static'
-
-	get '/' do
-		query='Trencin, Slovakia'
-		Weather=WeatherData.new
-		erb :index
-	end
-
-	post '/get_localities' do
-		localities=Weather.get_localities(params[:query])
-	end
-end
+DATA_DIR = 'data/'
+URLBASE = 'http://www.worldweatheronline.com'
+FORMAT = 'json'
+KEY = '31cedb71bb220950113101'
 
 class WeatherData
+  attr_accessor :locality
+  attr_accessor :observation_time, :cloudcover, :humidity, :precip_mm, :presure, :temp_c, :temp_f, :visibility, :weather_code, :weather_desc, 
+                :weather_icon_url, :winddir_16_point, :winddir_degree, :windspeed_kmph, :windspeed_miles 
 
-	attr_accessor :locality
-	attr_accessor :observation_time, :cloudcover, :humidity, :precip_mm, :presure, :temp_c, :temp_f, :visibility, :weather_code, :weather_desc, 
-								:weather_icon_url, :winddir_16_point, :winddir_degree, :windspeed_kmph, :windspeed_miles 
+  private
+    # Define a correct filename from location name
+    def normalize_locality(locality)
+      return locality.gsub!(/[^0-9A-Za-z.\-]/, ',')
+    end
 
-	private
-	  # Define a correct filename from location name
-		def normalize_locality(locality)
-		  return locality.gsub!(/[^0-9A-Za-z.\-]/, ',')
-		end
+  public
+    def initialize
+      @observation_time = ''
+      @cloudcover = ''
+      @humidity = ''
+      @precip_mm = ''
+      @presure = ''
+      @temp_c = ''
+      @temp_f = ''
+      @visibility = ''
+      @weather_code = ''
+      @weather_desc = ''
+      @weather_icon_url = ''
+      @winddir_16_point = ''
+      @winddir_degree = ''
+      @windspeed_kmph = ''
+      @windspeed_miles = ''
+    end
 
-	public
-		def initialize
-			@observation_time=''
-			@cloudcover=''
-			@humidity=''
-			@precip_mm=''
-			@presure=''
-			@temp_c=''
-			@temp_f=''
-			@visibility=''
-			@weather_code=''
-			@weather_desc=''
-			@weather_icon_url=''
-			@winddir_16_point=''
-			@winddir_degree=''
-			@windspeed_kmph=''
-			@windspeed_miles=''
-		end
+    # Read all lines with last residual value
+    def get_data(locality)
+      File.open(DATA_DIR+locality,"r").each do |line|
+        data = line.split("\t")
 
-		# Read all lines with last residual value
-		def get_data(locality)
-			File.open(DATA_DIR+locality,"r").each do |line|
-				data=line.split("\t")
+        @observation_time = data[0]
+        @cloudcover = data[7]
+        @humidity = data[4]
+        @precip_mm = data[6]
+        @presure = data[5]
+        @temp_c = data[2]
+        @temp_f = data[3]
+        @visibility = data[8]
+        @weather_code = data[1]
+        @weather_desc = data
+        @weather_icon_url = data[13][0..-2]
+        @winddir_16_point = data[9]
+        @winddir_degree = data[10]
+        @windspeed_kmph = data[11]
+        @windspeed_miles = data[12]
+      end
+      @locality = normalize_locality(locality)
+    end
 
-				@observation_time=data[0]
-				@cloudcover=data[7]
-				@humidity=data[4]
-				@precip_mm=data[6]
-				@presure=data[5]
-				@temp_c=data[2]
-				@temp_f=data[3]
-				@visibility=data[8]
-				@weather_code=data[1]
-				@weather_desc=data
-				@weather_icon_url=data[13][0..-2]
-				@winddir_16_point=data[9]
-				@winddir_degree=data[10]
-				@windspeed_kmph=data[11]
-				@windspeed_miles=data[12]
-			end
-			@locality=normalize_locality(locality)
-		end
+    def get_localities(query)
+      @url = "#{URLBASE}/feed/search.ashx?key=#{KEY}&q=#{query}&format=#{FORMAT}&num_of_days=2"
 
-		def get_localities(query)
-			@url="#{URLBASE}/feed/search.ashx?key=#{KEY}&q=#{query}&format=#{FORMAT}&num_of_days=2"
-
-			rawdata=''
-			open(@url).each do |line|
-				rawdata << line
-			end
-			return data=JSON.parse(rawdata)
-			
-#			@city=data["search_api"]["result"][0]["areaName"][0]["value"]
-#			@country=data["search_api"]["result"][0]["country"][0]["value"]
-#			@latitude=data["search_api"]["result"][0]["latitude"]
-#			@longitude=data["search_api"]["result"][0]["longitude"]
-#			@population=data["search_api"]["result"][0]["population"]
-#			@weatherUrl=data["search_api"]["result"][0]["weatherUrl"][0]["value"]
-		end	 
+      rawdata = ''
+      open(@url).each do |line|
+        rawdata << line
+      end
+      data = JSON.parse(rawdata)
+      return data["search_api"]["result"]
+      
+#      @city = data["search_api"]["result"][0]["areaName"][0]["value"]
+#      @country = data["search_api"]["result"][0]["country"][0]["value"]
+#      @latitude = data["search_api"]["result"][0]["latitude"]
+#      @longitude = data["search_api"]["result"][0]["longitude"]
+#      @population = data["search_api"]["result"][0]["population"]
+#      @weatherUrl = data["search_api"]["result"][0]["weatherUrl"][0]["value"]
+    end   
 end
 
+class WebWeatherGraph < Sinatra::Base
+  set :static, true
+  set :logging, true
+  set :public, File.dirname(__FILE__) + '/static'
+  set :views, File.dirname(__FILE__) + '/views'
 
-# get '/named_via_params/:argument' do
-#	"
-#	Using: '/named_via_params/:argument'<br/>
-#	params[:argument] -> #{params[:argument]} (Try changing it)
-#	"
-#end
-#
-# get '/named_via_block_parameter/:argument' do |argument|
-#    "
-#Using: '/named_via_block_parameter/:argument'<br/>
-#argument -> #{argument}
-#"
-#  end
-#
-#  get '/splat/*/bar/*' do
-#    "
-#Using: '/splat/*/bar/*'<br/>
-#params[:splat] -> #{params[:splat].join(', ')}
-#"
-#  end
-#
-#  get '/splat_extension/*.*' do
-#    "
-#Using: '/splat_extension/*.*'<br/>
-#filename -> #{params[:splat][0]}<br/>
-#extension -> #{params[:splat][1]}
-#"
-#  end
-#
-#  get %r{/regexp_params_captures/([\w]+)} do
-#    "params[:captures].first -> '#{params[:captures].first}'"
-#  end
-#
-#  get %r{/regexp_captures_via_block_parameter/([\w]+)} do |c|
-#    "c -> '#{c}'"
-#  end
+  before do
+    locality = "Trencin, Slovakia" 
+    @weather = WeatherData.new
+  end
 
+  get '/' do
+    erb :index
+  end
+
+  post '/get_localities' do
+   @localities = @weather.get_localities(params[:query])
+   erb :get_localities
+  end
+end
