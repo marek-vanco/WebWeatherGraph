888e1fc9625b4ad9618ba1d79904b2bca3767076
Marek Vanco <marek.vanco@laposte.net>
2011-02-15 23:42:49 +0100

    Preparation for collector yaml config file + selector layout - need be refactored

--- web_weather_graph.rb
+++ web_weather_graph.rb
@@ -3,10 +3,12 @@ require 'open-uri'
 require 'json'
 require 'erb'
 require 'parseconfig'
+require 'yaml'
 
 config = ParseConfig.new('./web_weather_graph.conf')
 KEY = config.params['datasource']['key']
 DATA_DIR = config.params['weather_graph']['datadir']
+COLLECTOR_CONF =  config.params['weather_graph']['collector_file']
 
 URLBASE = 'http://www.worldweatheronline.com'
 FORMAT = 'json'
@@ -65,6 +67,7 @@ class WeatherData
       @locality = normalize_locality(locality)
     end
 
+    # return hash of localities matches with query
     def get_localities(query)
       @url = "#{URLBASE}/feed/search.ashx?key=#{KEY}&q=#{query}&format=#{FORMAT}&num_of_days=2"
 
@@ -75,13 +78,19 @@ class WeatherData
       data = JSON.parse(rawdata)
       return data["search_api"]["result"]
       
-#      @city = data["search_api"]["result"][0]["areaName"][0]["value"]
-#      @country = data["search_api"]["result"][0]["country"][0]["value"]
-#      @latitude = data["search_api"]["result"][0]["latitude"]
-#      @longitude = data["search_api"]["result"][0]["longitude"]
-#      @population = data["search_api"]["result"][0]["population"]
-#      @weatherUrl = data["search_api"]["result"][0]["weatherUrl"][0]["value"]
+#      @city = data[0]["areaName"][0]["value"]
+#      @country = data[0]["country"][0]["value"]
+#      @latitude = data[0]["latitude"]
+#      @longitude = data[0]["longitude"]
+#      @population = data[0]["population"]
+#      @weatherUrl = data[0]["weatherUrl"][0]["value"]
     end   
+
+    # Add locality to COLLECTOR_FILE for collector script
+    def add_to_collector(locality)
+      p locality.to_yaml
+#      open(DATA_DIR+COLLECTOR_CONF,'w') {|f| YAML.dump(locality, f)}
+    end
 end
 
 class WebWeatherGraph < Sinatra::Base
@@ -90,24 +99,35 @@ class WebWeatherGraph < Sinatra::Base
   set :public, File.dirname(__FILE__) + '/static'
   set :views, File.dirname(__FILE__) + '/views'
 
+# Need I do this? -> I don't know... (to not create a new instance for all time)
   before do
-    @weather = WeatherData.new
+    unless defined? @weather
+      @weather = WeatherData.new
+    end
   end
 
   get '/' do  
-      if params[:locality] == nil 
-        locality = "Trencin,Slovakia" 
-      else
-        locality = params[:locality]
-      end
-      p locality
-      @weather.get_data(locality)
+#     if params[:locality] == nil 
+#        locality = "Trencin,Slovakia" 
+#      else
+       p  locality = params[:locality]
+       locality=nil 
+       params[:locality]=nil
+
+#      end
+#      p locality
+#      @weather.get_data(locality)
+#     @weather.add_to_collector(locality)
 
     erb :index
   end
 
   post '/get_localities' do
-   @localities = @weather.get_localities(params[:query])
-   erb :get_localities
+    @localities = @weather.get_localities(params[:query])
+    erb :get_localities
+  end
+
+  post 'select_locality' do
+    p locality = params[:locality]
   end
 end
